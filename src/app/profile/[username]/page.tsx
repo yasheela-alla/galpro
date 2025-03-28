65fbc61c560d3b2b8b5795fe357484859ed8995e
0xAy4sh <yasheela435@gmail.com>
2025-03-28 06:33:13 +0530

    Update page.tsx

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 import { featuredProjects } from "@/data/projects";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
