3cd61c70e815b26e8345bab442e6cca606281d14
0xAy4sh <yasheela435@gmail.com>
2025-04-03 17:07:50 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Hero from "@/components/Hero";
 import ProjectGrid from "@/components/ProjectGrid";
 import Footer from "@/components/Footer";
 import { featuredProjects } from "@/data/projects";
+import { Metadata } from "next";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col">
+    <main className="min-h-screen flex flex-col bg-black">
       <Header />
       <Hero />
       <ProjectGrid
